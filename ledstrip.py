7b09a298a0f27a36a7c2bd2375a4755226e4eb86
Jonas Wollweber <jonas.wollweber@anothermonday.com>
2020-01-05 15:07:54 +0100

    initial code commit

---
+++ ledstrip.py
@@ -0,0 +1,133 @@
+"""
+This file includes code copied and modified from:
+https://github.com/rpi-ws281x/rpi-ws281x-python/blob/master/examples/strandtest.py
+which is published under BSD 2-Clause License:
+https://github.com/rpi-ws281x/rpi-ws281x-python/blob/master/LICENSE
+
+BSD 2-Clause License
+
+Copyright (c) 2018, Pimoroni Ltd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+import time
+from neopixel import Adafruit_NeoPixel, Color
+
+class LedStrip():
+    def __init__(
+        self,
+        ledCount = 110,      # Number of LED pixels.
+        ledPin = 18,      # GPIO pin connected to the pixels (18 uses PWM, 10 uses SPI /dev/spidev0.0).
+        ledFreqHz = 800000,  # LED signal frequency in hertz (usually 800khz)
+        ledDma = 10,      # DMA channel to use for generating signal (try 10)
+        ledBrightness = 255,     # Set to 0 for darkest and 255 for brightest
+        ledInvert = False,   # True to invert the signal (when using NPN transistor level shift)
+        ledChannel = 0,       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+        clearStrip = True
+    ):
+        self._ledCount = ledCount
+        self._ledPin = ledPin
+        self._ledFreqHz = ledFreqHz
+        self._ledDma = ledDma
+        self._ledBrightness = ledBrightness
+        self._ledInvert = ledInvert
+        self._ledChannel = ledChannel
+        
+        self.strip = None
+        self.strip = self._setup()
+
+        if clearStrip:
+            self.clear()
+
+    def _setup(self):
+        """Setups the hardware strip"""
+        if self.strip is None:
+            strip = Adafruit_NeoPixel(self._ledCount, self._ledPin, self._ledFreqHz, self._ledDma, self._ledInvert, self._ledBrightness, self._ledChannel)
+            strip.begin()
+            return strip   
+
+    def clear(self, delay = 0):
+        """Turns off the whole strip"""
+        self.colorWipe(Color(0,0,0), delay)     
+
+    def turnOnLedsAt(self, color, indices):
+        """Turns on all leds defined in 'indices' with color specified """
+        for i in indices:
+            self.strip.setPixelColor(i, color)  
+        self.strip.show()
+
+    def colorWipe(self, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def theaterChase(self, color, wait_ms=50, iterations=10):
+        """Movie theater light style chaser animation."""
+        for j in range(iterations):
+            for q in range(3):
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i+q, color)
+                self.strip.show()
+                time.sleep(wait_ms/1000.0)
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i+q, 0)
+
+    def wheel(self, pos):
+        """Generate rainbow colors across 0-255 positions."""
+        if pos < 85:
+            return Color(pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+            pos -= 85
+            return Color(255 - pos * 3, 0, pos * 3)
+        else:
+            pos -= 170
+            return Color(0, pos * 3, 255 - pos * 3)
+
+    def rainbow(self, wait_ms=20, iterations=1):
+        """Draw rainbow that fades across all pixels at once."""
+        for j in range(256*iterations):
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, self.wheel((i+j) & 255))
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def rainbowCycle(self, wait_ms=20, iterations=5):
+        """Draw rainbow that uniformly distributes itself across all pixels."""
+        for j in range(256*iterations):
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, self.wheel((int(i * 256 / self.strip.numPixels()) + j) & 255))
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def theaterChaseRainbow(self, wait_ms=50):
+        """Rainbow movie theater light style chaser animation."""
+        for j in range(256):
+            for q in range(3):
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i+q, self.wheel((i+j) % 255))
+                self.strip.show()
+                time.sleep(wait_ms/1000.0)
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i+q, 0)
---
+++ wordclock.py
@@ -0,0 +1,140 @@
+import time
+import datetime
+import math
+from neopixel import Color
+import logging
+import signal
+from threading import Event
+from ledstrip import LedStrip
+
+class Wordclock():
+
+    def __init__(
+        self,
+        ledBrightness = 1,     # Set to 0 for darkest and 255 for brightest
+        ledColor = Color(72, 209, 204),
+        roundTimeDown = True   # True to round down minutes to last 5 minutes - False to round up to the next 5 minutes
+    ):
+        self._ledStrip = LedStrip(ledBrightness=ledBrightness)
+        self._roundTimeDown = roundTimeDown
+        self._oldNow = None
+        self._oldHour = None
+        self._oldMinute = None
+        self.ledColor = ledColor
+
+        if roundTimeDown:
+            self.rounder = math.floor
+        else:
+            self.rounder = math.ceil
+        
+        # LED INDICES
+        self._defaultLeds = list(range(0,2)) + list(range(3, 6)) # es + ist
+        self._clockLeds = list(range(99, 102))    # uhr
+        self._halfLeds = list(range(44, 48))      # halb
+        self._quarterLeds = list(range(26, 33))   # viertel
+        self._beforeLeds = list(range(41, 44))    # vor
+        self._afterLeds = list(range(33, 37))     # nach
+        self._fiveMinLeds = list(range(7, 11))    # fünf (1)
+        self._tenMinLeds = list(range(18, 22))    # zehn (1)
+        self._twentyMinLeds = list(range(11, 18)) # zwanzig
+        self._hourLeds = {
+            #3: (22, 26),
+            11: list(range(49, 52)),    # elf
+            5: list(range(51, 55)),     # fünf (2)
+            1: list(range(62, 66)),     # eins 
+            2: list(range(55, 59)),     # zwei
+            3: list(range(66, 70)),     # drei (2)
+            4: list(range(73, 77)),     # vier
+            6: list(range(83, 88)),     # sechs
+            8: list(range(77, 81)),     # acht 
+            7: list(range(88, 94)),     # sieben
+            12: list(range(94, 99)),    # zwölf
+            10: list(range(106, 110)),  # zehn (2)
+            9: list(range(103, 107)),   # neun
+        }
+        self._minuteToLeds = {
+            0: self._clockLeds,
+            5: self._fiveMinLeds + self._afterLeds,
+            10: self._tenMinLeds + self._afterLeds,
+            15: self._quarterLeds + self._afterLeds,
+            20: self._twentyMinLeds + self._afterLeds,
+            25: self._fiveMinLeds + self._beforeLeds + self._halfLeds,
+            30: self._halfLeds,
+            35: self._fiveMinLeds + self._afterLeds + self._halfLeds,
+            40: self._twentyMinLeds + self._beforeLeds,
+            45: self._quarterLeds + self._beforeLeds,
+            50: self._tenMinLeds + self._beforeLeds,
+            55: self._fiveMinLeds + self._beforeLeds
+        }
+
+        self._exitFlag = self._setupExitHandler()
+    
+    def _setupExitHandler(self):
+        for sig in ('TERM', 'HUP', 'INT'):
+            signal.signal(getattr(signal, 'SIG'+sig), self.quit)
+        return Event()
+
+    def quit(self, signo, _frame):
+        logging.info("Interrupted by {0}, shutting down".format(signo))
+        self._exitFlag.set()
+
+    def getNextUpdateTime(self):
+        oldRoundedNow = datetime.datetime(self._now.year, self._now.month, self._now.day, self._hour, self._minute)
+        if (self._roundTimeDown):
+            return oldRoundedNow + datetime.timedelta(minutes=5)
+        return oldRoundedNow
+    
+    def getRoundedTime(self, now=None):
+        if (now is None):
+            now = datetime.datetime.now()
+        self._now = now
+        self._hour = now.hour
+        self._minute = self.rounder(now.minute / 5) * 5
+
+    def _convertTimeToLedIndices(self):
+        ledIndices = list(self._defaultLeds) # copy the list
+        ledIndices += self._convertHourToLedIndices()
+        ledIndices += self._convertMinuteToLedIndices()
+        return ledIndices
+
+    def _convertHourToLedIndices(self):
+        hour = self._hour
+        if hour > 12:
+            hour = hour - 12
+        if self._minute <= 20:
+            return self._hourLeds[hour]
+        return self._hourLeds[hour + 1]
+
+    def _convertMinuteToLedIndices(self):
+        return self._minuteToLeds[self._minute]
+
+    def runClock(self):
+        while not self._exitFlag.is_set():
+            self.getRoundedTime()
+            logging.info("Time: {0} - Rounded: {1}:{2}".format(self._now, self._hour, self._minute))
+            ledIndices = self._convertTimeToLedIndices()
+            logging.debug("LEDs ({0}): {1}".format(len(ledIndices),ledIndices))
+            self._ledStrip.clear(10)
+            self._ledStrip.turnOnLedsAt(self.ledColor, ledIndices)
+            delay = (self.getNextUpdateTime() - datetime.datetime.now()).total_seconds()
+            self._exitFlag.wait(delay)
+        self.clear()
+
+    def clear(self):
+        if self._exitFlag is not None:
+            self._exitFlag.set()
+        if self._ledStrip is not None:
+            self._ledStrip.clear()
+
+if __name__ == '__main__':
+    clock = None
+    try:
+        logging.basicConfig(level=logging.DEBUG, filename='wordclock.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        clock = Wordclock(1, Color(100,200,100),True)
+        clock.runClock()
+    except Exception:
+        logging.exception("Unhandled Exception occured.")
+        if clock is not None:
+            clock.clear()
+
+
